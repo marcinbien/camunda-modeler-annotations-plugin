7c300bb2a95196485d81bf08375aafbf4ec0b45b
Marcin Bien <marcin@bien.pl>
2025-10-02 19:51:18 +0200

    Remove build files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-/** 
- * Menu entries, styles and scripts are all optional. Feel free to delete them 
- * here along their respective folders (client and dist for style) if you don't need them. 
- * */
-
 'use strict';
 
 module.exports = {
   name: 'Camunda Modeler Annotations Plugin',
-  script: './client/client-bundle.js',
+  script: './annotations-plugin.js',
 };
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
   mode: 'development',
   entry: './client/client.js',
   output: {
-    path: path.resolve(__dirname, 'client'),
-    filename: 'client-bundle.js'
+    path: path.resolve(__dirname),
+    filename: 'annotations-plugin.js'
   },
   devtool: 'cheap-module-source-map',
   plugins: [
